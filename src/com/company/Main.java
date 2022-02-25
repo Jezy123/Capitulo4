6f8c7e621aa40a22553dcaccc70a3f07cb65850b
Jezy123 <juanmapg321@gmail.com>
2022-02-25 12:44:56 +0100

    Rama Javadoc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner teclado=  new Scanner(System.in);
+        Scanner teclado =  new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
         int dato = teclado.nextInt();
         int vector[] = new int[dato];
@@ -16,7 +16,7 @@ public class Main {
             if (i%10 == 0) System.out.println();
             System.out.print(i + 1 + "\t");
         }
-        vector=generarPrimos(dato);
+        vector = generarPrimos(dato);
         System.out.println("\nVector de primos hasta:"+dato);
         for (int i = 0; i < vector.length; i++) {
             if (i%10 == 0) System.out.println();
