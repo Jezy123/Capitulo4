80c7ea19a8e5c26163ae7161b958eabe9e1ba0b7
Jezy123 <juanmapg321@gmail.com>
2022-02-28 12:22:29 +0100

    Refactorizar codigo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,6 @@
 package com.company;
 import java.util.Scanner;
-/**  @author JuanManuel
-@version 1.0
-*/
+
 public class Main {
 
     public static void main(String[] args) {
@@ -24,10 +22,6 @@ public class Main {
         }
     }
 
-    /**
-     * Este metodo se encarga de rellanar un array de numero primos con el limite de 1 a max un valor dado
-     * @return devuelve un array de primos o uno vacio
-     */
     // Generar números primos de 1 a max
     public static int[] generarPrimos (int max)
     {
